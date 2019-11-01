ebd31bd96eb9e5955b04cbfbf2c933ada5c5b2c4
Al-Kom <alexarkom99@gmail.com>
2019-11-01 21:01:57 +0300

    Add initialize()

--- mmemory.c
+++ mmemory.c
@@ -1,7 +1,9 @@
 #include "mmemory.h"
-/**
-	@
-**/
+#include <stdlib.h>
+
+//----------------------------------------------------------------------------------
+// memory structures
+//----------------------------------------------------------------------------------
 
 typedef struct Block
 {
@@ -12,7 +14,7 @@ typedef struct Block
     struct Block*      next;    //pointer to next memory block
 } Block;
 
-struct MemoryManager
+typedef struct MemoryManager
 {
     VA*				memory;			//memory to get user
 
@@ -20,9 +22,68 @@ struct MemoryManager
 
     struct Block*	first_block;	//pointer to first memory block
 
-};
+} MemoryManager;
 
-//	initialize flag
-int is_inited = 0;
-//	global manager
-MemoryManager manager;
+//----------------------------------------------------------------------------------
+// global variables
+//----------------------------------------------------------------------------------
+
+int is_inited = 0;      // initialize flag
+
+MemoryManager manager;  // global manager
+
+//----------------------------------------------------------------------------------
+// private functions
+//----------------------------------------------------------------------------------
+
+/**
+ 	@func	free_manager
+ 	@brief	Free memory, that was allocated  for manager, and clean data
+**/
+void free_manager(void)
+{
+    //free memory
+    manager.szMemory = 0;
+    if(manager.memory)
+    {
+        free(manager.memory);
+        manager.memory = 0;
+    }
+    //free blocks
+    Block
+            *current_block = manager.first_block,
+            *next_block = 0;
+    while(current_block)
+    {
+        next_block = current_block->next;
+        free(current_block);
+        current_block = next_block;
+    }
+    manager.first_block = 0;
+}
+
+//----------------------------------------------------------------------------------
+// library functions
+//----------------------------------------------------------------------------------
+
+int initialize(int n, int szPage)
+{
+    is_inited = 0;
+
+    //clean manager
+    free_manager();
+    //check arguments
+    if(n <= 0 || szPage <=0)
+        return -1;
+    //allocate user memory
+    manager.szMemory = (size_t) n*szPage;
+    manager.memory = malloc(sizeof(VA)*manager.szMemory);
+    if(!manager.memory)
+    {
+        free_manager();
+        return 1;
+    }
+
+    is_inited = 1;
+    return 0;
+}
